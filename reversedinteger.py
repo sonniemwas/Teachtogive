d8b464143fdbb41a1768568badcb082ec0b23636
sonniemwas <108232724+sonniemwas@users.noreply.github.com>
2024-10-26 16:58:33 +0300

    Update reversedinteger.py

--- reversedinteger.py
+++ reversedinteger.py
@@ -1,3 +1,8 @@
+'''Write a program that accepts a string as input, capitalizes the first letter of each
+word in the string, and then returns the result string.
+Examples:
+"hi"=> returns "Hi"
+"i love programming"=> returns "I Love Programming" '''
 def reverse_integer(n):
     reversed_str = str(abs(n))[::-1]
     reversed_int = int(reversed_str)
