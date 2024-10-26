87e0e54e9af51e18aa0f34924f85e3aa9fcea64c
sonniemwas <108232724+sonniemwas@users.noreply.github.com>
2024-10-26 16:57:41 +0300

    Update parangam.py

--- parangam.py
+++ parangam.py
@@ -1,3 +1,10 @@
+'''
+Write a Python function to check whether a string is pangram or not. (Assume
+the string passed in does not have any punctuation)
+Note: Pangrams are words or sentences containing every letter of the
+alphabet at least once. For example: "The quick brown fox jumps over the
+lazy dog
+'''
 def is_pangram(text):
     alphabet = set("abcdefghijklmnopqrstuvwxyz")
     text_letters = set(text.lower().replace(" ", ""))
